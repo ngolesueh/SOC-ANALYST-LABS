b7ae2aa3d6ee3dfe74c1009e8192a0a5ee4bd990
DIVINE NGOL ESUEH <de.ngolesueh@gmail.com>
2025-11-11 17:04:01 -0600

    commiting Core Install Powershell Script

---
+++ CoreInstalls.ps1
@@ -0,0 +1,11 @@
+# Python (scripting & automation used in many labs)
+winget install Python.Python.3.12
+
+# Visual Studio Code (editor for scripts/configs)
+winget install Microsoft.VisualStudioCode
+
+# Git (version control for your lab notes & code)
+winget install Git.Git
+
+# Windows Subsystem for Linux (Ubuntu for Linux-based tools)
+wsl --install -d Ubuntu
